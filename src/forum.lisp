26b19c9ec308192c2e38eced1ce57724f8b9ffac
Travis <tssund93@gmail.com>
2016-10-18 10:19:24 -0400

    set doctype so things display correctly

--- src/forum.lisp
+++ src/forum.lisp
@@ -63,18 +63,22 @@
 
 ;;; The basic format that every viewable page will follow.
 (define-html-macro :standard-page ((&key title) &body body)
-  `(:html
-     (:head (:title ,(concatenate 'string
-				  title (if (equal title "")
-					    ""
-					    " - ")
-				  *site-name*))
-	    ,@*head*)
-     (:body
-      (:div :class "container"
-	    ,@*header*
-	    (:h2 ,title)
-	    ,@body))))
+  `(progn
+     (html
+       (:doctype "html" "PUBLIC" "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"))
+     (html
+       (:html
+	 (:head (:title ,(concatenate 'string
+				      title (if (equal title "")
+						""
+						" - ")
+				      *site-name*))
+		,@*head*)
+	 (:body
+	  (:div :class "container"
+		,@*header*
+		(:h2 ,title)
+		,@body))))))
 
 ;;; this function creates and publishes page <name> at https://your-site.com/<name>
 (defmacro publish-page (name &body body)
