589e384c9d1b4e021395b6bd4c5dabb7eeac9a72
Travis <tssund93@gmail.com>
2016-10-24 21:59:38 -0400

    made locked page

--- src/forum.lisp
+++ src/forum.lisp
@@ -92,76 +92,88 @@
 
 ;;; web pages beyond here
 (publish-page index
-              ((standard-page
-                   (:title "")
-                 (:body (index-buttons)
-                        (threads-table *threads-query*)
-                        (:div :class "fake-copyright"
-                              (:raw *fake-copyright*))))))
+  ((standard-page
+       (:title "")
+     (:body (index-buttons)
+            (threads-table *threads-query*)
+            (:div :class "fake-copyright"
+                  (:raw *fake-copyright*))))))
 
 (setf (ningle:route *app* "/") #'index)
 
 (publish-page view-thread
-              ((standard-page
-                   (:title (get-thread-title (query-param "thread")))
-                 (:body (thread-buttons)
-                        (thread-dropdown)
-                        (posts-table "SELECT *
+  ((standard-page
+       (:title (get-thread-title (query-param "thread")))
+     (:body (thread-buttons)
+            (thread-dropdown)
+            (posts-table "SELECT *
                                       FROM posts
                                       WHERE ThreadID = ?"
-                                     (query-param "thread"))
-                        (thread-buttons)
-                        (:div :class "fake-copyright"
-                              (:raw *fake-copyright*))))))
+                         (query-param "thread"))
+            (thread-buttons)
+            (:div :class "fake-copyright"
+                  (:raw *fake-copyright*))))))
 
 (publish-page login
-              ((standard-page
-                   (:title "Login")
-                 (:body
-                  (:form :method "POST" :action "/b/login"
-                         (:input :name "username" :type "text")
-                         (:br)
-                         (:input :name "password" :type "password")
-                         (:br)
-                         (:input :name "Submit1" :type "submit" :value "Submit")
-                         (:input :type "button"
-                                 :value "Main Page"
-                                 :onclick "window.location='../'"))))))
+  ((standard-page
+       (:title "Login")
+     (:body
+      (:form :method "POST" :action "/b/login"
+             (:input :name "username" :type "text")
+             (:br)
+             (:input :name "password" :type "password")
+             (:br)
+             (:input :name "Submit1" :type "submit" :value "Submit")
+             (:input :type "button"
+                     :value "Main Page"
+                     :onclick "window.location='../'"))))))
 
 (publish-page new-reply
-              ((cond ((thread-locked-p (query-param "thread"))
-                      '(302 (:location "/locked")))
-                     (t (standard-page
-                            (:title "New Reply")
-                          (:body
-                           (:form :action (format nil "b/submitpost?thread=~d"
-                                                  (query-param "thread"))
-                                  :method "post"
-                                  (:textarea :id "postfield"
-                                             :name "Body"
-                                             :rows "7"
-                                             :class "col-xs-12"
-                                             :required t)
-                                  ;; (reply-buttons)
-                                  (image-upload-form))))))))
+  ((cond ((thread-locked-p (query-param "thread"))
+          '(302 (:location "/locked")))
+         (t (standard-page
+                (:title "New Reply")
+              (:body
+               (:form :action (format nil "b/submitpost?thread=~d"
+                                      (query-param "thread"))
+                      :method "post"
+                      (:textarea :id "postfield"
+                                 :name "Body"
+                                 :rows "7"
+                                 :class "col-xs-12"
+                                 :required t)
+                      ;; (reply-buttons)
+                      (image-upload-form))))))))
+
+(publish-page locked
+  ((standard-page
+       (:title "Thread Locked")
+     (:body (:p "And it's surely better that way.")
+            (:p (:button :class "btn btn-default btn-sm"
+                         :onclick "window.location='./'"
+                         "Back"))))))
+
+(publish-page new-thread
+  ((standard-page
+       (:title "New Thread"))))
 
 (publish-page b/login
-              ((let ((user-status nil))
-                 ;; if no status, user doesn't exist
-                 (if (setf user-status (get-user-status (query-param "username")))
-                     (let ((session-id nil))
-                       ;; find an id not in use and set it to session-id
-                       (loop while (gethash
-                                    (setf session-id (intern (write-to-string (make-v4-uuid))))
-                                    *sessions*))
+  ((let ((user-status nil))
+     ;; if no status, user doesn't exist
+     (if (setf user-status (get-user-status (query-param "username")))
+         (let ((session-id nil))
+           ;; find an id not in use and set it to session-id
+           (loop while (gethash
+                        (setf session-id (intern (write-to-string (make-v4-uuid))))
+                        *sessions*))
 
-                       ;; TODO: make it easier to query session variables
-                       (setf (gethash session-id *sessions*) (make-hash-table))
-                       (setf (gethash 'username (gethash session-id *sessions*)) (query-param "username"))
-                       (setf (gethash 'userstatus (gethash session-id *sessions*)) user-status)
-                       (setf (gethash 'userlastactive (gethash session-id *sessions*)) (get-universal-time))
+           ;; TODO: make it easier to query session variables
+           (setf (gethash session-id *sessions*) (make-hash-table))
+           (setf (gethash 'username (gethash session-id *sessions*)) (query-param "username"))
+           (setf (gethash 'userstatus (gethash session-id *sessions*)) user-status)
+           (setf (gethash 'userlastactive (gethash session-id *sessions*)) (get-universal-time))
 
-                       `(302 (:location "/"
-                                        :set-cookie ,(set-cookie "sessionid" session-id))))
-                     '(302 (:location "/loginfailed")))))
-              :method :POST)
+           `(302 (:location "/"
+                            :set-cookie ,(set-cookie "sessionid" session-id))))
+         '(302 (:location "/loginfailed")))))
+  :method :POST)
