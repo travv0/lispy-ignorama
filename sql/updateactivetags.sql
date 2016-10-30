5b68ee7343e1d84861fae8439c930737accf495f
Travis <tssund93@gmail.com>
2016-10-30 10:58:54 -0400

    update active tags

---
+++ sql/updateactivetags.sql
@@ -0,0 +1,16 @@
+UPDATE tags
+SET IsActive = false
+WHERE UserStatusID = (SELECT UserStatusID
+                      FROM UserStatuses
+                      WHERE UserStatusDesc = 'User');
+
+UPDATE tags
+SET TagName = 'Off-Topic'
+WHERE TagName = 'Random';
+
+UPDATE tags
+SET IsActive = true
+WHERE TagName IN ('Anime & Manga',
+                  'Video Games',
+                  'Technology',
+                  'Off-Topic');
